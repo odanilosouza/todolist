e353430b88d3b789c0f17fb759df81e81486ca19
Danilo Souza <dev.danilosouza@gmail.com>
2025-03-23 15:32:33 -0300

    first commit

---
+++ includes/conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "todolist";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+} else {
+    echo "Conexão com o banco de dados estabelecida com sucesso!";
+}
+?>
